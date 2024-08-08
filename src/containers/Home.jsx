8304baaa6f2209f2c1bb4ccddaa882d16490dd7f
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-08-08 17:34:26 +0500

    Forst changes

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,14 +1,25 @@
+/* eslint-disable import/extensions */
 // Init
 import React from 'react';
 
 // Importing Components
-import Header from '../components/Header';
+import Header from '../components/common/Header';
+import HeroSection from '../components/Home/Hero-sections';
+import ProductSlider from '../components/Home/Product-slider';
+import SocialLink from '../components/Home/Social-links';
+import CardsSaction from '../components/Home/Cards-section';
+import Footer from '../components/common/Footer';
 
 // Home Component
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
+      <ProductSlider />
+      <SocialLink />
+      <CardsSaction />
+      <Footer />
     </div>
   );
 }
