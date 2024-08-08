991df55c53cbc3c33b8233a58dd0a4f3ee8d5d77
Mobeen Ahmad <Mobeen0096@gmail.com>
2024-08-08 17:04:40 +0500

    First commit

---
+++ src/components/Home/Product-slider.jsx
@@ -0,0 +1,314 @@
+import React from 'react';
+
+function Productslider() {
+  return (
+    <div>
+      <div data-w-id="39e7d324-7206-8032-0a1c-5686af579281" className="section">
+        <div className="wrapper">
+          <div className="three-grid-wrapper">
+            <div className="clip">
+              <h1
+                style="
+                transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                  rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+                opacity: 1;
+                transform-style: preserve-3d;
+              "
+                className="large-heading"
+              >
+                Meet the glide
+                <span className="gradient-span">speakers team&nbsp;</span>
+              </h1>
+            </div>
+            <div
+              id="w-node-_23ac0eef-5eaf-a151-af17-587f8a5f44a5-f5583ba2"
+              className="max-450 clip"
+            >
+              <div
+                className="body-display large"
+                style="
+                transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                  rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+                transform-style: preserve-3d;
+                opacity: 1;
+              "
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, euismod felis, eros
+                mi purus in est. Vitae sodales tellus vitae, tincidunt in sed orci.
+              </div>
+            </div>
+            <div id="w-node-_1867ff73-25fb-e391-9d7e-cbeef5141f15-f5583ba2" className="clip">
+              <a
+                style="
+                transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                  rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+                opacity: 1;
+                transform-style: preserve-3d;
+              "
+                href="https://webflow-path-one.webflow.io/about"
+                className="button w-inline-block"
+              >
+                <div>Learn more about us</div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="team-collection-list-wrapper w-dyn-list">
+          <div role="list" className="team-wrapper w-dyn-items">
+            <div role="listitem" className="w-dyn-item">
+              <a
+                href="https://webflow-path-one.webflow.io/team/terry-von"
+                className="team-item w-inline-block"
+                style="
+                transform: translate3d(0.0079992vw, 0px, 0px)
+                  scale3d(0.8, 0.8, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                  skew(0deg, 0deg);
+                transform-style: preserve-3d;
+                opacity: 1;
+                will-change: transform;
+              "
+              >
+                <div className="slide-image team-image">
+                  <div
+                    style="
+                    background-image: url('https://assets.website-files.com/60593a2037760a299a3ee0ec/605a757a76a6a92776538dd1_Portrait011.jpeg');
+                  "
+                    className="speaker-image team-images"
+                  />
+                  <div className="team-member-info">
+                    <h1
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="large-heading"
+                    >
+                      Terry Von
+                    </h1>
+                    <div
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="body-display"
+                    >
+                      CMO
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div role="listitem" className="w-dyn-item">
+              <a
+                href="https://webflow-path-one.webflow.io/team/samantha-tai"
+                className="team-item w-inline-block"
+                style="
+                transform: translate3d(0.0079992vw, 0px, 0px)
+                  scale3d(0.8, 0.8, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                  skew(0deg, 0deg);
+                transform-style: preserve-3d;
+                opacity: 1;
+                will-change: transform;
+              "
+              >
+                <div className="slide-image team-image">
+                  <div
+                    style="
+                    background-image: url('https://assets.website-files.com/60593a2037760a299a3ee0ec/605a76128bff514e83e69f08_Portrait012.jpeg');
+                  "
+                    className="speaker-image team-images"
+                  />
+                  <div className="team-member-info">
+                    <h1
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="large-heading"
+                    >
+                      Samantha Tai
+                    </h1>
+                    <div
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="body-display"
+                    >
+                      CEO
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div role="listitem" className="w-dyn-item">
+              <a
+                href="https://webflow-path-one.webflow.io/team/craig-wents"
+                className="team-item w-inline-block"
+                style="
+                transform: translate3d(0.0079992vw, 0px, 0px)
+                  scale3d(0.8, 0.8, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                  skew(0deg, 0deg);
+                transform-style: preserve-3d;
+                opacity: 1;
+                will-change: transform;
+              "
+              >
+                <div className="slide-image team-image">
+                  <div
+                    style="
+                    background-image: url('https://assets.website-files.com/60593a2037760a299a3ee0ec/605a77be5435861244bbfe25_Portrait016.jpeg');
+                  "
+                    className="speaker-image team-images"
+                  />
+                  <div className="team-member-info">
+                    <h1
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="large-heading"
+                    >
+                      Craig Wents
+                    </h1>
+                    <div
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="body-display"
+                    >
+                      COO
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div role="listitem" className="w-dyn-item">
+              <a
+                href="https://webflow-path-one.webflow.io/team/jerry-maid"
+                className="team-item w-inline-block"
+                style="
+                transform: translate3d(0.0079992vw, 0px, 0px)
+                  scale3d(0.8, 0.8, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                  skew(0deg, 0deg);
+                transform-style: preserve-3d;
+                opacity: 1;
+                will-change: transform;
+              "
+              >
+                <div className="slide-image team-image">
+                  <div
+                    style="
+                    background-image: url('https://assets.website-files.com/60593a2037760a299a3ee0ec/605a76fede95ca6971b86fcc_Portrait014.jpeg');
+                  "
+                    className="speaker-image team-images"
+                  />
+                  <div className="team-member-info">
+                    <h1
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="large-heading"
+                    >
+                      Jerry Maid
+                    </h1>
+                    <div
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="body-display"
+                    >
+                      Customer Relationships
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div role="listitem" className="w-dyn-item">
+              <a
+                href="https://webflow-path-one.webflow.io/team/sarah-prat"
+                className="team-item w-inline-block"
+                style="
+                transform: translate3d(0.0079992vw, 0px, 0px)
+                  scale3d(0.8, 0.8, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                  skew(0deg, 0deg);
+                transform-style: preserve-3d;
+                opacity: 1;
+                will-change: transform;
+              "
+              >
+                <div className="slide-image team-image">
+                  <div
+                    style="
+                    background-image: url('https://assets.website-files.com/60593a2037760a299a3ee0ec/605a7758de8c17303882292a_Portrait015.jpeg');
+                  "
+                    className="speaker-image team-images"
+                  />
+                  <div className="team-member-info">
+                    <h1
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="large-heading"
+                    >
+                      Sarah Prat
+                    </h1>
+                    <div
+                      style="
+                      transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1)
+                        rotateX(0deg) rotateY(0deg) rotateZ(0deg)
+                        skew(0deg, 0deg);
+                      opacity: 1;
+                      transform-style: preserve-3d;
+                    "
+                      className="body-display"
+                    >
+                      Executive Vice President
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="background-shade third-height">
+          <div className="wrapper shade-wrapper" />
+          <div className="background-cover" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Productslider;
